b255a83de886727cbf40974feace65f12895cb41
jktunney <jktunney5@gmail.com>
2015-06-17 00:10:16 -0400

    adding codeschool

---
+++ js/trains.js
@@ -0,0 +1,8 @@
+// train loop
+var trainsOperational = 8;
+var trainNumber = 1;
+while ( trainNumber <= trainsOperational) {
+
+    console.log("Train #" + trainNumber + " is running");
+    trainNumber++;
+}
